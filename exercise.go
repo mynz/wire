30ffad33ac922c9d4fdefccbd76f155e7b233f30
mynz <rollingteapot@gmail.com>
2016-12-14 19:06:58 +0900

    wire/manager.go

--- exercise.go
+++ exercise.go
@@ -2,26 +2,46 @@ package main
 
 import (
 	"./wire"
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
 )
 
 func main() {
 	fmt.Println("Hello")
 
+	statusPath := "_status.json"
 	rootDir := "."
 	// rootDir := "D:/go"
 
-	fileSpecs := wire.CollectFileSpecs(rootDir)
-	fmt.Println(fileSpecs)
+	man := wire.NewManager(rootDir)
+	man.Evalueate()
+	man.SaveFile(statusPath)
 
-	// json.Marshal(fileSpecs)
-	bs, _ := json.MarshalIndent(fileSpecs, "", "  ")
-	fmt.Println(string(bs[:]))
-
-	statusPath := "_status.json"
-	if err := ioutil.WriteFile(statusPath, bs, 0666); err != nil {
-		panic(err)
-	}
+	/*
+	 *     if false {
+	 *         top := make([]wire.FileSpec, 0)
+	 *         bs, err := ioutil.ReadFile(statusPath)
+	 *         if err != nil {
+	 *             panic(err)
+	 *         }
+	 *
+	 *         if err := json.Unmarshal(bs, &top); err != nil {
+	 *             fmt.Println(err)
+	 *             panic(err)
+	 *         }
+	 *
+	 *         fmt.Println(top)
+	 *     }
+	 *
+	 *     if false {
+	 *         fileSpecs := wire.CollectFileSpecs(rootDir)
+	 *         fmt.Println(fileSpecs)
+	 *
+	 *         bs, _ := json.MarshalIndent(fileSpecs, "", "  ")
+	 *         fmt.Println(string(bs[:]))
+	 *
+	 *         if err := ioutil.WriteFile(statusPath, bs, 0666); err != nil {
+	 *             panic(err)
+	 *         }
+	 *     }
+	 */
 }
---
+++ wire/manager.go
@@ -0,0 +1,99 @@
+package wire
+
+import (
+	"crypto/sha512"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+type hashDigit [64]byte
+
+func (hash *hashDigit) String() string {
+	str := ""
+	for _, d := range hash {
+		str += fmt.Sprintf("%02x", d)
+	}
+	return str
+}
+
+////
+
+type FileSpec struct {
+	Path  string    `json:"path"`
+	Size  int64     `json:"size"`
+	MTime time.Time `json:"mtime"`
+	Hash  string    `json:"hash"`
+}
+
+func (fs FileSpec) String() string {
+	return fmt.Sprintf("path: %s, size: %d, Hash: %s...", fs.Path, fs.Size, fs.Hash[0:8])
+}
+
+////
+
+type Manager struct {
+	RootDir   string
+	FileSpecs []FileSpec
+}
+
+func NewManager(rootDir string) *Manager {
+	man := new(Manager)
+	man.RootDir = rootDir
+	return man
+}
+
+func collectFileSpecs(rootDir string) []FileSpec {
+	type Info struct {
+		Path     string
+		FileInfo os.FileInfo
+	}
+
+	infoList := make([]Info, 0)
+
+	filepath.Walk(rootDir, func(path string, info os.FileInfo, err error) error {
+		if info.IsDir() {
+			if info.Name() == ".git" {
+				return filepath.SkipDir
+			}
+		}
+		// check if it is a relgular file.
+		if info.Mode().IsRegular() {
+			fs := Info{Path: path, FileInfo: info}
+			infoList = append(infoList, fs)
+		}
+		return nil
+	})
+
+	specs := make([]FileSpec, 0, len(infoList))
+	for _, info := range infoList {
+		path := info.Path
+		size := info.FileInfo.Size()
+		mtime := info.FileInfo.ModTime()
+		if bs, err := ioutil.ReadFile(path); err != nil {
+			panic(err)
+		} else {
+			hex := hashDigit(sha512.Sum512(bs))
+			hstr := hex.String()
+			specs = append(specs, FileSpec{path, size, mtime, hstr})
+		}
+	}
+	return specs
+}
+
+func (man *Manager) Evalueate() {
+	man.FileSpecs = collectFileSpecs(man.RootDir)
+}
+
+func (man *Manager) SaveFile(path string) error {
+	bs, _ := json.MarshalIndent(man.FileSpecs, "", "  ")
+	fmt.Println(string(bs[:]))
+
+	if err := ioutil.WriteFile(path, bs, 0666); err != nil {
+		return err
+	}
+	return nil
+}
