beff42d578cd6ab9eb50bfea35c00883b22b9fe4
mynz <rollingteapot@gmail.com>
2016-12-14 19:43:53 +0900

    html

--- httpd.go
+++ httpd.go
@@ -1,12 +1,28 @@
 package main
 
 import (
+	"html/template"
 	"net/http"
+	"time"
 )
+
+type Page struct {
+	Now time.Time
+}
 
 func main() {
 
 	handler := func(w http.ResponseWriter, r *http.Request) {
+		tmpl, err := template.ParseFiles("html/top.html")
+		if err != nil {
+			panic(err)
+		}
+
+		page := Page{time.Now()}
+		err = tmpl.Execute(w, page)
+		if err != nil {
+			panic(err)
+		}
 	}
 
 	http.HandleFunc("/", handler)
