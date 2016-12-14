c0d7e25dc3abbb53a29684ee820f32870448dc9b
mynz <rollingteapot@gmail.com>
2016-12-14 19:22:38 +0900

    snap

--- exercise.go
+++ exercise.go
@@ -20,33 +20,4 @@ func main() {
 
 	man.Evaluate()
 	man.SaveFile(statusPath)
-
-	/*
-	 *     if false {
-	 *         top := make([]wire.FileSpec, 0)
-	 *         bs, err := ioutil.ReadFile(statusPath)
-	 *         if err != nil {
-	 *             panic(err)
-	 *         }
-	 *
-	 *         if err := json.Unmarshal(bs, &top); err != nil {
-	 *             fmt.Println(err)
-	 *             panic(err)
-	 *         }
-	 *
-	 *         fmt.Println(top)
-	 *     }
-	 *
-	 *     if false {
-	 *         fileSpecs := wire.CollectFileSpecs(rootDir)
-	 *         fmt.Println(fileSpecs)
-	 *
-	 *         bs, _ := json.MarshalIndent(fileSpecs, "", "  ")
-	 *         fmt.Println(string(bs[:]))
-	 *
-	 *         if err := ioutil.WriteFile(statusPath, bs, 0666); err != nil {
-	 *             panic(err)
-	 *         }
-	 *     }
-	 */
 }
