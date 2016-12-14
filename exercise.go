675f23578960f0b3d5c86f6ab3e2e3c7969836c8
mynz <rollingteapot@gmail.com>
2016-12-14 14:58:07 +0900

    FileSpec

--- exercise.go
+++ exercise.go
@@ -1,20 +1,40 @@
 package main
 
 import (
+	"crypto/sha512"
 	"fmt"
 	"io/ioutil"
 	"os"
 	"path/filepath"
-	// "strings"
+	// "strconv"
 )
+
+type HashDigit [64]byte
+
+func (hash *HashDigit) String() string {
+	str := ""
+	for _, d := range hash {
+		str += fmt.Sprintf("%02x", d)
+	}
+	return str
+}
+
+////
+
+type FileSpec struct {
+	Path       string
+	FileInfo   os.FileInfo
+	HashString string
+}
+
+func NewFileSpec(path string, fi os.FileInfo) *FileSpec {
+	return &FileSpec{path, fi, ""}
+}
+
+////
 
 func main() {
 	fmt.Println("Hello")
-
-	type FileSpec struct {
-		Path     string
-		FileInfo os.FileInfo
-	}
 
 	fileSpecs := make([]FileSpec, 0)
 
@@ -26,9 +46,8 @@ func main() {
 		}
 		// check if it is a relgular file.
 		if info.Mode().IsRegular() {
-			fileSpecs = append(fileSpecs, FileSpec{path, info})
+			fileSpecs = append(fileSpecs, *NewFileSpec(path, info))
 		}
-
 		return nil
 	})
 
@@ -42,8 +61,13 @@ func main() {
 			panic(err)
 		} else {
 			fmt.Println(len(bs))
+
+			hex := HashDigit(sha512.Sum512(bs))
+			fmt.Println("hex", hex)
+
+			hstr := hex.String()
+			fmt.Println(hstr)
+
 		}
-
 	}
-
 }
